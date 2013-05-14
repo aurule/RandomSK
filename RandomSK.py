1730c089d6189482f2b48d558e878704196c23d7
Peter Andrews <pmandrews@gmail.com>
2013-05-13 22:57:04 -0400

    Updated internal documentation.

--- RandomSK.py
+++ RandomSK.py
@@ -4,38 +4,41 @@ from gi.repository import Gtk, Gdk
 
 import pairdefs
 
-# Define the gui and its actions.
 class Picker:
+    '''RandomSK UI and related functions.'''
     def pick_rand(self, widget=None, data=None):
+        '''Pick a random sequence from our current pairdict and set output text.'''
         self.disp.set_text(self.pairdict.pick())
 
     def copy(self, widget, data=None):
+        '''Copy displayed text to the system clipboard.'''
         self.clipboard.set_text(self.disp.get_text(), -1)
 
-    def delete_event(self, widget, event, data=None):
-        return False
-
     def destroy(self, widget, data=None):
+        '''Quit function.'''
         Gtk.main_quit()
 
     def update_title(self):
+        '''Update title string to reflect current pairdict name.'''
         self.title = "RandomSK - %s" % self.key
         self.window.set_title(self.title)
     
     def choose_pair(self, widget=None, data=None):
+        '''Store selected pairdict name and pairdict.'''
         self.key = self.pairs_store[self.picker.get_active_iter()][0]
         self.pairdict = self.pairs[self.key]
         self.update_title()
         
     def __init__(self):
+        '''Set up UI and internal vars.'''
         self.builder = Gtk.Builder()
         self.builder.add_from_file("randomsk.ui")
-        self.title = "RandomSK"
+        self.title = "RandomSK" #initial title string
         self.pairs = pairdefs.pairs
         
         # get refs to every part we'll be manipulating
-        self.disp = self.builder.get_object("output_text")
-        self.picker = self.builder.get_object("pair_select")
+        self.disp = self.builder.get_object("output_text") #text field for output
+        self.picker = self.builder.get_object("pair_select") #combobox for choosing pairdict
         
         # set up the picker's label store and attach
         self.pairs_store = Gtk.ListStore(str)
@@ -58,14 +61,14 @@ class Picker:
         # create a clipboard for easy copying
         self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
         
-        #set our initial title and display the lot
+        # set our initial title and display the lot
         self.window = self.builder.get_object("main_window")
         self.window.set_title(self.title)
         self.window.show_all()
         
-        # set the value for our dropdown, activating update triggers
+        # set the initial value for our dropdown, activating update triggers
         self.picker.set_active_iter(self.pairs_store[0].iter)
-        self.pick_rand()
+        self.pick_rand() #pick immediately
 
 def main():
     Gtk.main()
@@ -74,5 +77,5 @@ def main():
 # If the program is run directly or passed as an argument to the python
 # interpreter then create a Picker instance and show it
 if __name__ == "__main__":
-    choose = Picker()
+    rsk = Picker()
     main()
--- pairdefs.py
+++ pairdefs.py
@@ -1,19 +1,25 @@
 from random import choice
 
 class sk_heirarchy:
+    '''Picks pairs on two levels, with the second derived from the first.'''
     def __init__(self, data):
+        '''Data needs to be a dict of lists.'''
         self.data = data
     
     def pick(self):
+        '''One random element is chosen from the data keys, then a second element is picked from the list named by that key.'''
         seeming = choice(self.data.keys())
         kith = choice(self.data[seeming])
         return seeming+' '+kith
 
 class sk_flat:
+    '''Picks pairs without regard for levels.'''
     def __init__(self, data):
+        '''Data needs to be a list of lists.'''
         self.data = data
     
     def pick(self):
+        '''One random element will be picked from each list in our data.'''
         output = []
         for d in self.data:
             output.append(choice(d))
